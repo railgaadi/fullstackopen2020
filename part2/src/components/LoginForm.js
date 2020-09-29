5f5c514f259f93d9bd0f8c6a37a2322200baa39d
railgaadi <railgaadi420@gmail.com>
2020-09-29 12:54:10 +0530

    finish part 5 update part 2

---
+++ part2/src/components/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const LoginForm = ({ attemptLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  LoginForm.propTypes = {
+    attemptLogin: PropTypes.func.isRequired,
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    attemptLogin({ username, password });
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <h2>Login to add notes</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          username{" "}
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+            name="username"
+          />
+        </div>
+        <div>
+          password{" "}
+          <input
+            type="text"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
